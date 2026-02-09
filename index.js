9e4560714b84326814b632e252f76a835d3cd329
kIwwwww1 <danil29070@gmail.com>
2026-02-09 19:27:14 +0300

    base js

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
-let username
+// let username
 
-document.getElementById('send_button').onclick = function() {
-    username = document.getElementById('user_input').value
-    document.getElementById('welcome_message').textContent = `Welcome, ${username}!`
-    console.log(username)
+// document.getElementById('send_button').onclick = function() {
+//     username = document.getElementById('user_input').value
+//     document.getElementById('welcome_message').textContent = `Welcome, ${username}!`
+//     console.log(username)
+// }
+
+// ===============================
+
+// let age = '10'
+// let x = 'pizza'
+// let y = 'pizza'
+// let z = 'pizza'
+
+// age = Number(age)
+
+// x = Number(x)
+// y = String(y)
+// z = Boolean(z)
+
+// console.log(x, typeof x)
+// console.log(y, typeof y)
+// console.log(z, typeof z)
+
+// console.log(age + 1)
+
+// ===============================
+
+// const PI = 3.14159
+// let radius
+// let circumference
+
+// document.getElementById('radiusSubm').onclick = function() {
+//     radius = document.getElementById('new_myinput').value
+//     radius = Number(radius)
+//     circumference = 2 * PI * radius
+//     document.getElementById('result').textContent = `Длина окружности равна ${circumference}cm`
+// }
+
+// ===============================
+
+const minus = document.getElementById('minus')
+const plus = document.getElementById('plus')
+const reset = document.getElementById('reset')
+const result = document.getElementById('counter')
+let counter = 0
+
+minus.onclick = function() {
+    counter -= 1
+    result.textContent = counter
+}
+
+plus.onclick = function() {
+    counter += 1
+    result.textContent = counter
+}
+
+reset.onclick = function() {
+    counter = 0
+    result.textContent = counter
 }
